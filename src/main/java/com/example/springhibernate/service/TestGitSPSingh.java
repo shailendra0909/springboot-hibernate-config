2a35aca08bea8c59eb45d79634db6c5dccaee5e6
Shailendra <shailendra.cs09@gmail.com>
2023-08-20 14:40:27 +0530

    added sout

--- src/main/java/com/example/springhibernate/service/TestGitSPSingh.java
+++ src/main/java/com/example/springhibernate/service/TestGitSPSingh.java
@@ -3,5 +3,6 @@ package com.example.springhibernate.service;
 public class TestGitSPSingh {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
     }
 }
